fd9647f5627ae7983bdc672e9032dfb67b97bf19
doyedele1 <demilade.oyedele@gmail.com>
2022-05-01 00:36:35 +0100

    worked-on-the-3d-component

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -30,13 +30,13 @@ const VoxelDog = () => {
             const scW = container.clientWidth;
             const scH = container.clientHeight;
 
-            renderer.setSize(scW, scH)
+            renderer.setSize(scW, scH);
         }
     }, [renderer]);
 
     /* eslint-disable react-hooks/exhaustive-deps */
     useEffect(() => {
-        const { current: container } = refContainer
+        const { current: container } = refContainer;
         if (container && !renderer) {
             const scW = container.clientWidth;
             const scH = container.clientHeight;
@@ -45,85 +45,81 @@ const VoxelDog = () => {
                 antialias: true,
                 alpha: true
             });
-            renderer.setPixelRatio(window.devicePixelRatio)
-            renderer.setSize(scW, scH)
-            renderer.outputEncoding = THREE.sRGBEncoding
-            container.appendChild(renderer.domElement)
-            setRenderer(renderer)
+            renderer.setPixelRatio(window.devicePixelRatio);
+            renderer.setSize(scW, scH);
+            renderer.outputEncoding = THREE.sRGBEncoding;
+            container.appendChild(renderer.domElement);
+            setRenderer(renderer);
+        
+            const scale = scH * 0.005 + 4.8;
+            const camera = new THREE.OrthographicCamera(
+                -scale,
+                scale,
+                scale,
+                -scale,
+                0.01,
+                50000
+            );
+            camera.position.copy(initialCameraPosition);
+            camera.lookAt(target);
+            setCamera(camera);
 
-        // 640 -> 240
-        // 8   -> 6
-        const scale = scH * 0.005 + 4.8
-        const camera = new THREE.OrthographicCamera(
-            -scale,
-            scale,
-            scale,
-            -scale,
-            0.01,
-            50000
-        )
-        camera.position.copy(initialCameraPosition)
-        camera.lookAt(target)
-        setCamera(camera)
+            const ambientLight = new THREE.AmbientLight(0xcccccc, 1);
+            scene.add(ambientLight);
 
-        const ambientLight = new THREE.AmbientLight(0xcccccc, 1)
-        scene.add(ambientLight)
+            const controls = new OrbitControls(camera, renderer.domElement);
+            controls.autoRotate = true;
+            controls.target = target;
+            setControls(controls);
 
-        const controls = new OrbitControls(camera, renderer.domElement)
-        controls.autoRotate = true
-        controls.target = target
-        setControls(controls)
+            loadGLTFModel(scene, '/dog.glb', {
+                receiveShadow: false,
+                castShadow: false
+            }).then(() => {
+                animate()
+                setLoading(false)
+            });
 
-        loadGLTFModel(scene, '/dog.glb', {
-            receiveShadow: false,
-            castShadow: false
-        }).then(() => {
-            animate()
-            setLoading(false)
-        })
+            let req = null;
+            let frame = 0;
+            const animate = () => {
+                req = requestAnimationFrame(animate);
 
-        let req = null
-        let frame = 0
-        const animate = () => {
-            req = requestAnimationFrame(animate)
+                frame = frame <= 100 ? frame + 1 : frame;
 
-            frame = frame <= 100 ? frame + 1 : frame
+                if (frame <= 100) {
+                    const p = initialCameraPosition;
+                    const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20;
 
-            if (frame <= 100) {
-            const p = initialCameraPosition
-            const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20
+                    camera.position.y = 10;
+                    camera.position.x = p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed);
+                    camera.position.z = p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed);
+                    camera.lookAt(target);
+                } else {
+                    controls.update();
+                }
 
-            camera.position.y = 10
-            camera.position.x =
-                p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed)
-            camera.position.z =
-                p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed)
-            camera.lookAt(target)
-            } else {
-            controls.update()
+                renderer.render(scene, camera);
             }
 
-            renderer.render(scene, camera)
+            return () => {
+                // console.log('unmount');
+                cancelAnimationFrame(req);
+                renderer.dispose();
+            }
         }
-
-        return () => {
-            console.log('unmount')
-            cancelAnimationFrame(req)
-            renderer.dispose()
-        }
-        }
-    }, [])
+    }, []);
 
     useEffect(() => {
-        window.addEventListener('resize', handleWindowResize, false)
+        window.addEventListener('resize', handleWindowResize, false);
         return () => {
-        window.removeEventListener('resize', handleWindowResize, false)
+            window.removeEventListener('resize', handleWindowResize, false);
         }
-    }, [renderer, handleWindowResize])
+    }, [renderer, handleWindowResize]);
 
     return (
         <DogContainer ref={refContainer}>{loading && <DogSpinner />}</DogContainer>
     )
-    }
+}
 
 export default VoxelDog;
