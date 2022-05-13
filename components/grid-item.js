99db9148a6708545cde0fa330a527d5a487620f7
doyedele1 <demilade.oyedele@gmail.com>
2022-05-13 22:58:22 +0100

    cleaning-code

--- components/grid-item.js
+++ components/grid-item.js
@@ -24,13 +24,13 @@ export const GridItem = ({ children, href, title, thumbnail }) => (
 
 export const WorkGridItem = ({ children, id, title, thumbnail }) => (
     <Box w="100%" textAlign="center">
-        <NextLink href={`/works/${id}`} scroll={false}>
+        <NextLink href={`/works/${id}`} passHref scroll={false}>
             <LinkBox cursor="pointer">
                 <Image
-                src={thumbnail}
-                alt={title}
-                className="grid-item-thumbnail"
-                placeholder="blur"
+                    src={thumbnail}
+                    alt={title}
+                    className="grid-item-thumbnail"
+                    placeholder="blur"
                 />
                 <LinkOverlay href={`/works/${id}`}>
                     <Text mt={2} fontSize={20}>
